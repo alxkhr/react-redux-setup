679a7e9fda0da70b600113d7dd74238550fb40ae
Alexander Kehr <alex@alexanderkehr.de>
2020-11-24 15:19:13 +0100

    class vs hooks

--- src/components/login/login.tsx
+++ src/components/login/login.tsx
@@ -1,20 +1,42 @@
-import React from 'react';
+import React, { ChangeEvent, Component } from 'react';
 
 import { LoginService } from './login.service';
 
-export function Login(props: { onLogin: () => void }) {
-  return (
-    <div>
-      please log in
-      <br />
-      <button
-        onClick={() => {
-          LoginService.login('max');
-          props.onLogin();
-        }}
-      >
-        login
-      </button>
-    </div>
-  );
+interface LoginProps {
+  onLogin: () => void;
+}
+
+interface LoginState {
+  username: string;
+}
+
+export class Login extends Component<LoginProps, LoginState> {
+  constructor(props: LoginProps) {
+    super(props);
+    this.state = { username: '' };
+    this.onChangeUsername = this.onChangeUsername.bind(this);
+    this.onLogin = this.onLogin.bind(this);
+  }
+
+  onChangeUsername(event: ChangeEvent<HTMLInputElement>) {
+    this.setState({ username: event.target.value });
+  }
+
+  onLogin() {
+    LoginService.login(this.state.username);
+    this.props.onLogin();
+  }
+
+  render() {
+    return (
+      <div>
+        please log in
+        <br />
+        username:
+        <br />
+        <input type="text" value={this.state.username} onChange={this.onChangeUsername} />
+        <button onClick={this.onLogin}>login</button>
+      </div>
+    );
+  }
 }
